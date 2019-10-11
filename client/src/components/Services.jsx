7e937f339c7e71ac2fc4ee737c046112ddb4d094
Shelly <pxshelly@gmail.com>
2019-10-11 14:10:48 -0700

    Implement Services component

---
+++ client/src/components/Services.jsx
@@ -0,0 +1,128 @@
+import React from 'react';
+
+function services() {
+  return(
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Color (includes cut)</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Full Color</td>
+            <td>$55 and up</td>
+          </tr>
+          <tr>
+            <td>Highlights</td>
+            <td>$70 and up</td>
+          </tr>
+          <tr>
+            <td>Balayage/Ombre</td>
+            <td>$150 and up</td>
+          </tr>
+          <tr>
+            <td>Root Touch Up</td>
+            <td>$50</td>
+          </tr>
+        </tbody>
+      </table>
+      <table>
+        <thead>
+          <tr>
+            <th>Perm (includes cut)</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Regular</td>
+            <td>$50 and up</td>
+          </tr>
+          <tr>
+            <td>Digital</td>
+            <td>$100 and up</td>
+          </tr>
+          <tr>
+            <td>Straight</td>
+            <td>$140 and up</td>
+          </tr>
+        </tbody>
+      </table>
+      <table>
+        <thead>
+          <tr>
+            <th>Other</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Up-do</td>
+            <td>$40</td>
+          </tr>
+          <tr>
+            <td>Treatment</td>
+            <td>$35 and up</td>
+          </tr>
+        </tbody>
+      </table>
+      <table>
+        <thead>
+          <tr>
+            <th>Women</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Haircut</td>
+            <td>$18 and up</td>
+          </tr>
+          <tr>
+            <td>Kids (9 and under)</td>
+            <td>$15 and up</td>
+          </tr>
+          <tr>
+            <td>with shampoo</td>
+            <td>+ $4</td>
+          </tr>
+          <tr>
+            <td>Shampoo and blow dry</td>
+            <td>$15</td>
+          </tr>
+        </tbody>
+      </table>
+      <table>
+        <thead>
+          <tr>
+            <th>Men</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Haircut</td>
+            <td>$12</td>
+          </tr>
+          <tr>
+            <td>Kids (9 and under)</td>
+            <td>$10</td>
+          </tr>
+          <tr>
+            <td>with shampoo</td>
+            <td>+ $3</td>
+          </tr>
+          <tr>
+            <td>Full Color (includes cut)</td>
+            <td>$40</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default services;
