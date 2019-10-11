9bdaedcc93687715698b6abb25a38bcfe7446ec0
Shelly <pxshelly@gmail.com>
2019-10-11 12:50:29 -0700

    Spin up server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '../dist/')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+})
+
+app.listen(port, () => console.log(`Server listening on ${port}`));
