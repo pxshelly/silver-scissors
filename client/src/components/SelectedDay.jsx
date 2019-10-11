aceb9c0be9451dbb39c67ef4e18c290a5d4b9054
Shelly <pxshelly@gmail.com>
2019-10-11 10:40:04 -0700

    Render times of day

--- client/src/components/SelectedDay.jsx
+++ client/src/components/SelectedDay.jsx
@@ -1,11 +1,36 @@
 import React from 'react'; 
+{/* <li key={i}>{`${i}:00`}</li> */}
 
-function SelectedDay(props) {
-  return (
-    <div>
-      <h2>{props.day}</h2>
-    </div>
-  )
+class SelectedDay extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  timesOfDay() {
+    const times = [12];
+    for(let i = 1; i < 12; i++) {
+      times.push(i);
+    }
+    const am = times.map((time, i) => {
+      return <li key={i+1}>{`${time}:00 AM`}</li>;
+    })
+    const pm = times.map((time, i) => {
+      return <li key={i*13}>{`${time}:00 PM`}</li>;
+    })
+    return [...am, ...pm];
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>{this.props.day}</h2>
+        <ul>
+          {this.timesOfDay()}
+        </ul>
+      </div>
+    )
+
+  }
 }
 
 export default SelectedDay;
