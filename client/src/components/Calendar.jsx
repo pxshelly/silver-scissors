9cad985416c442bf5053b8810546593abaac0def
Shelly <pxshelly@gmail.com>
2019-10-10 22:29:57 -0700

    Render last and next months
      - refactor code into a loadCalendar method
      - calendar only changes one month at a time
      - only rerenders based on current month in time

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -5,38 +5,63 @@ class Calendar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentMonth: moment().format('MMMM'),
-      today: moment().date(),
-      currentYear: moment().year()
+      currentMonthAndYear: moment().format('MMMM YYYY'),
+      selectMonth: false,
+      currentMonth: moment().month()+1,
+      firstDayOfMonth: moment().startOf('month').format('d'),
+      daysInMonth: moment().daysInMonth(),
+      counter: 0
     };
+    // this.selectMonth = this.selectMonth.bind(this);
+    this.loadCalendar = this.loadCalendar.bind(this);
+    this.lastMonth = this.lastMonth.bind(this);
+    this.nextMonth = this.nextMonth.bind(this);
   }
 
-  firstDayOfMonth() {
-    return moment().startOf('month').format('d');
-  }
-
-  render() {
-    // create td tags for each weekday
-    const weekdays = moment.weekdays().map((day, i) => {
+  getWeekdays() {
+     const weekdays = moment.weekdays().map((day, i) => {
       return <td key={i}>{day}</td>;
     })
+    return weekdays;
+  }
+  
+  lastMonth() {
+    this.setState({
+      counter: this.state.counter++,
+      currentMonth: this.state.currentMonth--,
+      currentMonthAndYear: moment().subtract(this.state.counter, 'month').format('MMMM YYYY'),
+      firstDayOfMonth: moment().subtract(this.state.counter, 'month').startOf('month').format('d'),
+      daysInMonth: moment().subtract(this.state.counter, 'month').daysInMonth()
+    })
+    this.loadCalendar();
+  }
 
+  nextMonth() {
+    this.setState({
+      counter: this.state.counter++,
+      currentMonth: this.state.currentMonth++,
+      currentMonthAndYear: moment().add(this.state.counter, 'month').format('MMMM YYYY'),
+      firstDayOfMonth: moment().add(this.state.counter, 'month').startOf('month').format('d'),
+      daysInMonth: moment().add(this.state.counter, 'month').daysInMonth()
+    })
+    this.loadCalendar();
+  }
+
+  loadCalendar() {
     // determine which day the first of the month falls in and how many blank spaces to leave in calendar
     const blanks = [];
-    for (let i = 0; i < this.firstDayOfMonth(); i++) {
+    for (let i = 0; i < this.state.firstDayOfMonth; i++) {
       blanks.push(<td key={i*1000}></td>)
     }
 
     // create td tags for each day in month
     const allDays = []
-    const days = moment().daysInMonth();
-    for (let i = 1; i <= days; i++) {
+    for (let i = 1; i <= this.state.daysInMonth; i++) {
       allDays.push(<td key={i}><span>{i}</span></td>)
     }
 
     // combine blank spaces with number of days and create rows for each week
     const totalCells = [...blanks, ...allDays];
-    console.log(totalCells);
     const calendarRows = [];
     let calendarCells = [];
     totalCells.forEach((day, i) => {
@@ -51,23 +76,46 @@ class Calendar extends React.Component {
         calendarRows.push(calendarCells.slice());
       }
     })
-
+  
     // create <td> tag for each row with random key
     const weeks = calendarRows.map((week, i) => {
       return <tr key={i*100}>{week}</tr>
     }); 
 
+    return weeks;
+  }
+
+  // selectMonth() {
+  //   this.setState({selectMonth: !this.state.selectMonth});
+  //   const months = moment.months().map((month) => {
+  //     return <div key={month}>{month}</div>
+  //   })
+  //   console.log(months);
+  //   if (this.state.selectMonth) {
+  //     return (
+  //       <div>
+  //         {months}
+  //       </div>
+  //     )
+  //   }
+  // }
+
+  render() {
     return (
       <div className='calendar-container'>
         <table>
           <thead>
             <tr>
               <th>{this.state.currentYear}</th>
-              <th>{this.state.currentMonth}</th>
             </tr>
-            <tr>{weekdays}</tr>
+            <tr>
+              <td onClick={() => this.lastMonth()}>Last Month</td>
+              <th onClick={() => this.selectMonth()}>{this.state.currentMonthAndYear}</th>
+              <td onClick={() => this.nextMonth()}>Next Month</td>
+            </tr>
+            <tr>{this.getWeekdays()}</tr>
           </thead>
-          <tbody>{weeks}</tbody>
+          <tbody>{this.loadCalendar()}</tbody>
         </table>
       </div>
     )
