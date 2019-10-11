64d9f4ae6c325c91f1ef3279ec2452404f4e3582
Shelly <pxshelly@gmail.com>
2019-10-11 10:58:49 -0700

    Render AddAppointment component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header.jsx';
 import Calendar from './Calendar.jsx';
+import AddAppointment from './AddAppointment.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -12,6 +13,7 @@ class App extends React.Component {
       <div>
         <Header />
         <Calendar />
+        <AddAppointment />
       </div>
     );
   }
