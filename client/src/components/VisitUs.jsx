996e03dec5f13b3801cbafd88e2c1777c040cb7e
Shelly <pxshelly@gmail.com>
2019-10-11 15:19:28 -0700

    Implement VisitUs component

---
+++ client/src/components/VisitUs.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function visitUs() {
+  return (
+    <div>
+      Silver Scissors Hair Salon
+      <address>
+        4559 Peck Rd
+        <br/>
+        El Monte, CA 91732
+      </address>
+      (626) 444-3856
+      <br/>
+      Tuesday - Sunday
+      <br/>
+      9AM - 7PM
+      <br/>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3304.3055633691974!2d-118.0190540845859!3d34.087311823328186!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2d9ff23167abd%3A0xccc05f30d476c6b5!2sSilver%20Scissors%20Hair%20Salon!5e0!3m2!1sen!2sus!4v1570830352263!5m2!1sen!2sus"></iframe>
+    </div>
+  )
+}
+
+export default visitUs;
