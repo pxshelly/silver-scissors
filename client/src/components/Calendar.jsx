42d6456cf867caedb41edfb82cc029225ad82e09
Shelly <pxshelly@gmail.com>
2019-10-21 21:43:09 -0700

    Fix bug in Calendar
      - update year
      - render months dynamically

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import moment from 'moment';
 import SelectedDate from './SelectedDate.jsx';
+import { runInThisContext } from 'vm';
 
 class Calendar extends React.Component {
   constructor(props) {
@@ -10,47 +11,56 @@ class Calendar extends React.Component {
       month: moment().format('MMMM'),
       firstDayOfMonth: moment().startOf('month').format('d'),
       daysInMonth: moment().daysInMonth(),
-      counter: 0,
+      monthDifference: 0,
+      yearDifference: 0,
       isDateSelected: false,
       selectedDate: 'Select a date'
     };
     this.loadCalendar = this.loadCalendar.bind(this);
     this.lastMonth = this.lastMonth.bind(this);
     this.nextMonth = this.nextMonth.bind(this);
+    this.updateState = this.updateState.bind(this);
     this.selectDate = this.selectDate.bind(this);
   }
 
   getWeekdays() {
      const weekdays = moment.weekdaysShort().map((day, i) => {
       return <td key={i}>{day}</td>;
-    })
+    });
     return weekdays;
   }
-  
-  lastMonth() {
+
+  updateState() {
     this.setState({
-      counter: this.state.counter++,
-      month: moment().subtract(this.state.counter, 'month').format('MMMM'),
-      firstDayOfMonth: moment().subtract(this.state.counter, 'month').startOf('month').format('d'),
-      daysInMonth: moment().subtract(this.state.counter, 'month').daysInMonth()
-    })
+      month: moment().add(this.state.monthDifference, 'month').format('MMMM'),
+      firstDayOfMonth: moment().add(this.state.monthDifference, 'month').startOf('month').format('d'),
+      daysInMonth: moment().add(this.state.monthDifference, 'month').daysInMonth(),
+    });
     this.loadCalendar();
+  }
+
+  lastMonth() {
+    // determine if year needs to be updated also    
+    if (this.state.month === 'January' && this.state.monthDifference < 0 || this.state.month === 'January' && this.state.yearDifference > 0) {
+      this.setState({
+        yearDifference: this.state.yearDifference -= 1,
+        year: moment().add(this.state.yearDifference, 'years').format('YYYY')});
+    };
+    // update month
+    this.setState({monthDifference: this.state.monthDifference -= 1});
+    this.updateState();
   }
 
   nextMonth() {
-    this.setState({
-      counter: this.state.counter++,
-      month: moment().add(this.state.counter, 'month').format('MMMM'),
-      firstDayOfMonth: moment().add(this.state.counter, 'month').startOf('month').format('d'),
-      daysInMonth: moment().add(this.state.counter, 'month').daysInMonth()
-    })
-    this.loadCalendar();
-  }
-
-  selectDate(e) {
-    this.setState({
-      isDateSelected: true,
-      selectedDate: e.target.innerText})
+    // determine if year needs to be updated also
+    if (this.state.month === 'December' && this.state.monthDifference > 0 || this.state.month === 'December' && this.state.yearDifference < 0) {
+      this.setState({
+        yearDifference: this.state.yearDifference += 1,
+        year: moment().add(this.state.yearDifference, 'years').format('YYYY')});
+    };
+    // update month
+    this.setState({monthDifference: this.state.monthDifference += 1});
+    this.updateState();
   }
 
   loadCalendar() {
@@ -58,13 +68,13 @@ class Calendar extends React.Component {
     const blanks = [];
     for (let i = 0; i < this.state.firstDayOfMonth; i++) {
       blanks.push(<td key={i*1000}></td>)
-    }
+    };
 
     // create td tags for each day in month
     const allDays = []
     for (let i = 1; i <= this.state.daysInMonth; i++) {
       allDays.push(<td key={i} onClick={(e) => this.selectDate(e)}><span>{i}</span></td>)
-    }
+    };
 
     // combine blank spaces with number of days and create rows for each week
     const totalCells = [...blanks, ...allDays];
@@ -81,7 +91,7 @@ class Calendar extends React.Component {
       if (i === totalCells.length - 1) {
         calendarRows.push(calendarCells.slice());
       }
-    })
+    });
   
     // create <td> tag for each row with random key
     const weeks = calendarRows.map((week, i) => {
@@ -91,14 +101,21 @@ class Calendar extends React.Component {
     return weeks;
   }
 
+  selectDate(e) {
+    this.setState({
+      isDateSelected: true,
+      selectedDate: e.target.innerText
+    });
+  }
+
   render() {
     return (
       <div>
         <div className='calendar-year'>{this.state.year}</div>
         <div className='calendar-header'>
-          <span className='calendar-header-contents' onClick={() => this.lastMonth()}>Last Month</span>
-          <span className='calendar-header-contents' onClick={() => this.selectMonth()}>{this.state.month}</span>
-          <span className='calendar-header-contents' onClick={() => this.nextMonth()}>Next Month</span>
+          <span className='calendar-header-contents' onClick={() => this.lastMonth()}>{'<'}</span>
+          <span className='calendar-header-contents'>{this.state.month}</span>
+          <span className='calendar-header-contents' onClick={() => this.nextMonth()}>{'>'}</span>
         </div>
         <table className='calendar-table'>
           <tbody>
