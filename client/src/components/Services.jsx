f5f560b73438a8e87e02c28dbc418abe6c6a897f
Shelly <pxshelly@gmail.com>
2019-10-22 00:19:20 -0700

    Render services dynamically

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -1,128 +1,73 @@
 import React from 'react';
 
 function services() {
-  return(
+  const services = {
+    Color: {
+      'Full Color': '$55 and up',
+      'Highlights': '$70 and up',
+      'Balayage/Ombre': '$150 and up',
+      'Root Touch Up': '$50'
+    },
+    Perm: {
+      'Regular': '$50 and up',
+      'Digital': '$100 and up',
+      'Straight': '$140 and up'
+    },
+    Other: {
+      'Up-do': '$40',
+      'Treatment': '$35 and up'
+    },
+    Women: {
+      'Haircut': '$18 and up',
+      'Kids (9 and under)': '$15 and up',
+      'with shampoo': '+ $4',
+      'Shampoo and blow dry': '$15'
+    },
+    Men: {
+      'Haircut': '$12',
+      'Kids (9 and under)': '$10',
+      'with shampoo': '+ $3',
+      'Full Color (includes cut)': '$40'
+    },
+  };
+
+  const tables = [];
+
+  const createTableRows = (table) => {
+    const keys = Object.keys(table);
+    const values = Object.values(table);
+    const tableRows = keys.map((key, i) => {
+      return (
+        <tr key={i}>
+          <td key={key}>{key}</td>
+          <td key={values[i]}>{values[i]}</td>
+        </tr>
+      );
+    });
+    return tableRows;
+  };
+  
+  for (let key in services) {
+    tables.push(
+      <table key={key}>
+        <thead>
+          <tr>
+            <th>{key}</th>
+            <th>{'Price'}</th>
+          </tr>
+        </thead>
+        <tbody>
+      {createTableRows(services[key])}
+        </tbody>
+      </table>
+    );
+  }
+
+  return (
     <div className='services-container'>
-      <table>
-        <thead>
-          <tr>
-            <th>Color (includes cut)</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Full Color</td>
-            <td>$55 and up</td>
-          </tr>
-          <tr>
-            <td>Highlights</td>
-            <td>$70 and up</td>
-          </tr>
-          <tr>
-            <td>Balayage/Ombre</td>
-            <td>$150 and up</td>
-          </tr>
-          <tr>
-            <td>Root Touch Up</td>
-            <td>$50</td>
-          </tr>
-        </tbody>
-      </table>
-      <table>
-        <thead>
-          <tr>
-            <th>Perm (includes cut)</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Regular</td>
-            <td>$50 and up</td>
-          </tr>
-          <tr>
-            <td>Digital</td>
-            <td>$100 and up</td>
-          </tr>
-          <tr>
-            <td>Straight</td>
-            <td>$140 and up</td>
-          </tr>
-        </tbody>
-      </table>
-      <table>
-        <thead>
-          <tr>
-            <th>Other</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Up-do</td>
-            <td>$40</td>
-          </tr>
-          <tr>
-            <td>Treatment</td>
-            <td>$35 and up</td>
-          </tr>
-        </tbody>
-      </table>
-      <table>
-        <thead>
-          <tr>
-            <th>Women</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Haircut</td>
-            <td>$18 and up</td>
-          </tr>
-          <tr>
-            <td>Kids (9 and under)</td>
-            <td>$15 and up</td>
-          </tr>
-          <tr>
-            <td>with shampoo</td>
-            <td>+ $4</td>
-          </tr>
-          <tr>
-            <td>Shampoo and blow dry</td>
-            <td>$15</td>
-          </tr>
-        </tbody>
-      </table>
-      <table>
-        <thead>
-          <tr>
-            <th>Men</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Haircut</td>
-            <td>$12</td>
-          </tr>
-          <tr>
-            <td>Kids (9 and under)</td>
-            <td>$10</td>
-          </tr>
-          <tr>
-            <td>with shampoo</td>
-            <td>+ $3</td>
-          </tr>
-          <tr>
-            <td>Full Color (includes cut)</td>
-            <td>$40</td>
-          </tr>
-        </tbody>
-      </table>
+      {tables}
     </div>
-  )
+  );
 }
 
 export default services;
