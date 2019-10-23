575b932025c366dbda68309c19b81e0b0ca6df29
Shelly <pxshelly@gmail.com>
2019-10-23 12:10:38 -0700

    Modularize services

---
+++ client/src/components/services.js
@@ -0,0 +1,29 @@
+const services = {
+  Color: {
+    'Women Full Color': '$55 and up',
+    'Men Full Color' : '$40',
+    'Highlights': '$70 and up',
+    'Balayage/Ombre': '$150 and up',
+    'Root Touch Up': '$50'
+  },
+  Perm: {
+    'Regular Perm': '$50 and up',
+    'Digital Perm': '$100 and up',
+    'Straight Perm': '$140 and up'
+  },
+  Haircut: {
+    'Women Haircut': '$18 and up',
+    'Men Haircut': '$12',
+    'Girls Haircut (9 and under)': '$15 and up',
+    'Boys (9 and under)': '$10'
+  },
+  Other: {
+    'Shampoo and blow dry': '$15',
+    'Treatment': '$35 and up',
+    'Up-do': '$40',
+    'Women Shampoo': '+ $4',
+    'Men Shampoo': '+ $3'
+  }
+};
+
+module.exports = { services };
