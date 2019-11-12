6e6db70139e8e33df2d17e53144a88d1b44df14d
Shelly <pxshelly@gmail.com>
2019-11-12 04:45:35 -0800

    Update schema
      - add users table
      - remove request table

--- database/schema.sql
+++ database/schema.sql
@@ -4,28 +4,30 @@ CREATE DATABASE silver_scissors;
 
 \c silver_scissors; 
 
-CREATE TABLE appointments (
-  appt_id SERIAL PRIMARY KEY,
-  customer_name TEXT NOT NULL, 
-  stylist TEXT NOT NULL,
-  hair_service TEXT NOT NULL,
-  appt_date DATE NOT NULL,
-  appt_time TIME NOT NULL,
-  telephone VARCHAR NOT NULL,
-  textable TEXT NOT NULL,
-  notes TEXT NOT NULL,
-  pictures TEXT
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  user_name VARCHAR NOT NULL,
+  user_type VARCHAR NOT NULL,
+  fb_id VARCHAR NULL,
+  fb_access_token VARCHAR NULL,
+  email VARCHAR NULL,
+  telephone BIGINT NULL
 );
 
-CREATE TABLE requests (
-  appt_id SERIAL PRIMARY KEY,
-  customer_name TEXT NOT NULL, 
-  stylist TEXT NOT NULL,
-  hair_service TEXT NOT NULL,
+CREATE TABLE appointments (
+  id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES users(id) NULL,
+  customer_name VARCHAR NOT NULL, 
+  stylist VARCHAR NOT NULL,
+  hair_service VARCHAR NOT NULL,
   appt_date DATE NOT NULL,
   appt_time TIME NOT NULL,
-  telephone VARCHAR NOT NULL,
-  textable TEXT NOT NULL,
-  notes TEXT NOT NULL,
-  pictures TEXT
+  telephone BIGINT NOT NULL,
+  textable VARCHAR NOT NULL,
+  notes VARCHAR NULL,
+  pictures VARCHAR NULL,
+  price SMALLINT NULL,
+  duration VARCHAR NULL,
+  appt_status VARCHAR NULL,
+  approved_by VARCHAR NULL
 );
