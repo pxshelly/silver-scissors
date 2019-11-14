2a69fc32b4e9e6cadd406b0c812e47b6d62ca4e0
Shelly <pxshelly@gmail.com>
2019-11-13 16:03:51 -0800

    Add endpoints for Facebook authentication
      - update endpoints to use query parameters

--- server/index.js
+++ server/index.js
@@ -3,16 +3,35 @@ const app = express();
 const port = process.env.PORT || 3000;
 const path = require('path');
 const bodyParser = require('body-parser');
-const { getAppts, getApptDetails, makeAppt, editAppt, removeAppt } = require('./controllers');
+const passport = require('passport');
+const session = require('express-session');
+const { getApptsByDate, getAppt, makeAppt, editAppt, removeAppt } = require('./controllers');
+const { setUpPassport, isLoggedIn } = require('./passport');
 
+setUpPassport();
+
+// middleware
 app.use(express.static(path.join(__dirname, '../dist/')));
 app.use(bodyParser.json());
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 } }))
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/schedule/:date', getAppts);
-app.post('/appointment', makeAppt);
-app.get('/appointment/:id', getApptDetails);
-app.put('/appointment/update/:id', editAppt);
-app.delete('/appointment/delete/:id', removeAppt);
+// facebook endpoints
+app.get('/auth/facebook', passport.authenticate('facebook'));
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+  }));
+
+// app endpoints
+app.get('/schedule/:date', getApptsByDate);
+app.get('/appointments', getAppt);
+
+app.post('/appointments', makeAppt);
+app.put('/appointments', editAppt);
+app.delete('/appointment/:id', removeAppt);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
