d74b87fb409c5be129cf8defd5b78783bcdba826
Shelly <pxshelly@gmail.com>
2019-11-14 21:45:14 -0800

    Add sign in button

--- client/src/components/shared/Header.jsx
+++ client/src/components/shared/Header.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
-function header() {
+function header(props) {
   return (
-    <div className='title-container'>
-      <h1>Silver Scissors</h1>
-      <h2>hair salon</h2>
+    <div className='header-container'>
+      <button type='button' className='sign-in-button'>
+        {props.signedIn ? 'Welcome' : 'Sign In'}
+          <a href="/auth/facebook" />
+      </button>
+      <div className='title-container'>
+        <h1>Silver Scissors</h1>
+        <h2>hair salon</h2>
+      </div>
     </div>
   )
-} 
+}
 
 export default header;
