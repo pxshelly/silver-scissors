6cc5e401030e85daf4861950bad3ccc68f99ef0b
Shelly <pxshelly@gmail.com>
2019-11-25 15:49:56 -0800

    Refactor rendering of service tables

--- client/src/components/customer/ServiceTables.jsx
+++ client/src/components/customer/ServiceTables.jsx
@@ -1,52 +1,145 @@
 import React from 'react';
-import { services } from '../shared/services';
 
-function serviceTables() {
-  const createTableRows = (table) => {
-    const keys = Object.keys(table);
-    const values = Object.values(table);
-
-    const tableRows = keys.map((key, i) => {
-      return (
-        <tr key={i}>
-          <td key={key}>{key}</td>
-          <td key={values[i]}>{values[i]}</td>
-        </tr>
-      );
-    });
-    return tableRows;
-  };
-
-  const tables = [];
-
-  for (let key in services) {
-    if (key.includes('Image')) {
-      tables.push(
-        <img src={services[key]} />);
-    } else {
-      tables.push(
-        <table key={key} className='table-pairs'>
+const serviceTables = () => {
+  return (
+    <div className='service-tables-container'>
+      <div className='haircut-table'>
+        <table>
           <thead>
             <tr>
-              <th>{key}</th>
-              <th>{'Price'}</th>
+              <th>Haircut</th>
+              <th>Price</th>
             </tr>
           </thead>
           <tbody>
-            {createTableRows(services[key])}
+            <tr>
+              <td>Women</td>
+              <td>$18 and up</td>
+            </tr>
+            <tr>
+              <td>Men</td>
+              <td>$12</td>
+            </tr>
+            <tr>
+              <td>Girls (9 and under)</td>
+              <td>$15 and up</td>
+            </tr>
+            <tr>
+              <td>Boys (9 and under)</td>
+              <td>$10</td>
+            </tr>
           </tbody>
         </table>
-      );
-    }
-  }
+        <div className='service-image'>
+          <img src='https://silver-scissors.s3-us-west-1.amazonaws.com/IMG_5751.jpg' />
+          <div className='table-img-overlay-right'>
+            <div className='table-img-overlay-text'>Haircuts</div>
+          </div>
+        </div>
+      </div>
+      <div className='color-table'>
+        <div className='service-image'>
+          <img src='https://silver-scissors.s3-us-west-1.amazonaws.com/IMG_5836.jpg' />
+          <div className='table-img-overlay-left'>
+            <div className='table-img-overlay-text'>Coloring</div>
+          </div>
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>Color</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Women</td>
+              <td>$55 and up</td>
+            </tr>
+            <tr>
+              <td>Men</td>
+              <td>$40</td>
+            </tr>
+            <tr>
+              <td>Highlights</td>
+              <td>$70 and up</td>
+            </tr>
+            <tr>
 
-  for (let i = 0; i < tables.length; i += 3) {
-    tables.splice(i, 0, <br key={i} />);
-  }
-
-  return (
-    <div className='service-tables-container'>
-      {tables}
+              <td>Balayage/Ombre</td>
+              <td>$150 and up</td>
+            </tr>
+            <tr>
+              <td>Root Touch Up</td>
+              <td>$50</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className='perm-table'>
+        <table>
+          <thead>
+            <tr>
+              <th>Perm</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Regular</td>
+              <td>$50 and up</td>
+            </tr>
+            <tr>
+              <td>Digital</td>
+              <td>$100 and up</td>
+            </tr>
+            <tr>
+              <td>Straight</td>
+              <td>$140 and up</td>
+            </tr>
+          </tbody>
+        </table>
+        <div className='service-image'>
+          <img src='https://silver-scissors.s3-us-west-1.amazonaws.com/IMG_5859.jpg' />
+          <div className='table-img-overlay-left'>
+            <div className='table-img-overlay-text'>Perms</div>
+          </div>
+        </div>
+      </div>
+      <div className='additional-table'>
+        <div className='service-image'>
+          <img src='https://silver-scissors.s3-us-west-1.amazonaws.com/IMG_5912.jpg' />
+          <div className='table-img-overlay-left'>
+            <div className='table-img-overlay-text'>Additional Services</div>
+          </div>
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>Additional</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Shampoo and blow dry</td>
+              <td>$15</td>
+            </tr>
+            <tr>
+              <td>Treatment</td>
+              <td>$35 and up</td>
+            </tr>
+            <tr>
+              <td>Up-do</td>
+              <td>$40</td>
+            </tr>
+            <tr>
+              <td>Shampoo</td>
+              <td>$4</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
