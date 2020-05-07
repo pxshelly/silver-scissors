516f9fa3e99f8ba2419e4eac3e6cb8142c742bfa
Shelly <pxshelly@gmail.com>
2020-05-07 00:21:00 -0700

    Add login modal

--- client/src/components/shared/LogIn.jsx
+++ client/src/components/shared/LogIn.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 
-const authenticate = () => {
-  location.href = 'http://localhost:3000/auth/facebook';
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: false
+    }
+  }
+
+  render() {
+    if (!document.cookie) {
+      return (
+        <div className='modal'>
+          <div className='modal-content'>Please sign in
+          <a href='/auth/facebook'>
+            <button type='button' className='modal-content sign-in-button' onClick={this.props.authenticate}>
+                {this.state.loggedIn ? 'Welcome' : 'Sign In'}
+              </button>
+            </a>
+          </div>
+        </div>
+      );
+    }
+    return null;
+  }
 }
 
-const logIn = () => {
-  return (
-    <div>
-      <button type='button' onClick={authenticate}>Sign In</button>
-    </div>
-  );
-}
-
-export default logIn;
+export default Login;
