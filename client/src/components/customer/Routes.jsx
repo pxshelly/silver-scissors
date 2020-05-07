e8e3827039f3fa93988632d1cdc7bafac3aa8a67
Shelly <pxshelly@gmail.com>
2020-05-06 23:12:15 -0700

    Add login routes for customer

--- client/src/components/customer/Routes.jsx
+++ client/src/components/customer/Routes.jsx
@@ -2,12 +2,22 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import ServiceTables from './ServiceTables.jsx';
 import VisitUs from './VisitUs.jsx';
+import ApptForm from './ApptForm.jsx';
+import ApptPage from './ApptPage.jsx';
+import Login from '../shared/Login.jsx';
+import Logout from '../shared/Logout.jsx';
+import Homepage from '../shared/Homepage.jsx'
 
 function Routes() {
   return (
     <Switch>
+      <Route exact path="/" component={Homepage} />
       <Route exact path="/services" component={ServiceTables} />
+      <Route exact path="/request-appointment" component={ApptForm} />
       <Route exact path="/visit-us" component={VisitUs} />
+      <Route exact path="/my-appointments" component={ApptPage} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/logged-out" component={Logout} />
     </Switch>
   )
 }
