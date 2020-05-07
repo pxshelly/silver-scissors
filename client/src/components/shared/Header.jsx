2c441e6d3b0cfa9c750d82bd297745cd6586da81
Shelly <pxshelly@gmail.com>
2020-05-07 00:24:11 -0700

    Add links to sign in and sign out

--- client/src/components/shared/Header.jsx
+++ client/src/components/shared/Header.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
+import axios from 'axios';
 
-function header(props) {
-  return (
-    <div className='header-container'>
-      <button type='button' className='sign-in-button'>
-        {props.signedIn ? 'Welcome' : 'Sign In'}
-          <a href="/auth/facebook" />
-      </button>
-      <div className='title-container'>
-        <h1>Silver Scissors</h1>
-        <h2>hair salon</h2>
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: false
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3000/login-status') 
+      .then((result) => {
+        const { data: { loggedIn = false } = {} } = result;
+        if (loggedIn) {
+          this.setState({loggedIn: true})
+        }
+      })
+  }
+
+  render() {
+    return (
+      <div className='header-container'>
+        <a href='/' className='title-container'>
+          <h1>Silver Scissors</h1>
+          <h2>hair salon</h2>
+        </a>
+        <a href='/auth/facebook'>{this.state.loggedIn ? 'Welcome' : 'Sign In'}</a>
+        <a href='/logout'>Sign Out</a>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
-export default header;
+export default Header;
