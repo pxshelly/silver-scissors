3a8cf87d57f68fb0abac25e921b412029e98db06
Shelly <pxshelly@gmail.com>
2020-05-06 23:09:55 -0700

    Update error handling

--- client/src/components/customer/ApptPage.jsx
+++ client/src/components/customer/ApptPage.jsx
@@ -8,25 +8,27 @@ class ApptPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      appts: [],
-      pendingAppts: [],
-      error: ''
+      appts: []
     }
   }
 
   componentDidMount() {
-    let customer_id = 2;
-    axios.get(`/appointments/${customer_id}`)
+    // let user_id = 2;
+    axios.get(`/appointments?user_id=${user_id}`)
       .then((result) => this.setState({appts: result.data}))
-      .catch((error) => this.setState({error: error}));
+      .catch((error) => {
+        if (error.response.status === 302) {
+          location.href = error.response.data.redirect;
+        }
+      });;
   }
 
   render() {
     return (
       <div>
-        <UpcomingAppts appts={this.state.appts}/>
-        <PendingAppts appts={this.state.pendingAppts}/>
-        <PastAppts appts={this.state.appts}/>
+        <UpcomingAppts appts={this.state.appts} />
+        <PendingAppts appts={this.state.pendingAppts} />
+        <PastAppts appts={this.state.appts} />
       </div>
     );   
   }
