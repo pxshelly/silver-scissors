36178cb71d8543568938771f970c1722bbca3308
Shelly <pxshelly@gmail.com>
2020-05-06 22:50:33 -0700

    Update deleteAppt method
      - will edit db

--- client/src/components/employee/ApptDetails.jsx
+++ client/src/components/employee/ApptDetails.jsx
@@ -3,18 +3,22 @@ import moment from 'moment';
 import axios from 'axios';
 
 function apptDetails(props) {
-  const { customer_name, stylist, hair_service, appt_date, appt_time, telephone, textable, notes, pictures, appt_id } = props.apptDetails;
+  const { customer_name, stylist, hair_services, appt_date, appt_time, email, telephone, textable, notes, pictures, id, price, duration_hours, duration_minutes} = props.apptDetails;
 
   const details = {
     'Customer Name': customer_name,
+    'Service': hair_services,
     'Stylist': stylist,
-    'Service': hair_service,
     'Date': moment(appt_date).format('LL'),
     'Time': appt_time, 
+    'Email': email,
     'Telephone': telephone,
     'Can we text this number?': textable,
     'Notes': notes,
     'Pictures': pictures,
+    'Price': price,
+    'Hours': duration_hours,
+    'Minutes': duration_minutes
   }
 
   const tableRows = [];
@@ -32,33 +36,52 @@ function apptDetails(props) {
     setTimeout(() => {
       let details = props.apptDetails;
       for (const key in details) {
-        if (key === 'appt_id') {
+        if (details[key] === null) {
+          continue;
+        }
+        if (key === 'id' || key === 'user_id' || key === 'appt_status') {
           continue;
         } 
         if (key === 'appt_date') {
           details[key] = moment(details[key]).format('YYYY-MM-DD');        
         }
-        document.getElementsByName(key)[0].value = details[key];
+        if (key === 'appt_time') {
+          details[key] = moment(details[key], 'h:mm A').format('HH:mm:ss');
+        }
+        if (key === 'telephone') {
+          let telephone = details[key].toString().split('');
+          telephone.splice(3, 0, '-' );
+          telephone.splice(7, 0, '-' );
+          details[key] = telephone.join('');
+        }
+        if (key === 'textable') {
+          if (details[key] === 'true') {
+            document.getElementById('yes').checked = true;
+            continue;
+          } else {
+            document.getElementById('no').checked = true;
+            continue;
+          }
+        }
+        document.getElementById(key).value = details[key];
       }
     }, 10);
   }
 
   const deleteAppt = () => {
-    let id = document.getElementsByClassName('delete-appt-button')[0].getAttribute('id');
-    axios.delete(`/appointment/delete/${id}`)
-      .catch((error) => console.log(error));
+    axios.put(`/appointments?status=deleted`, props.apptDetails)
   }
 
   if (props.apptDetails) {
     return (
       <div>
-        <button id={appt_id} className='edit-appt-button' onClick={() => autofill()}>Edit Appointment</button>
+        <button id={id} className='edit-appt-button' onClick={autofill}>Edit Appointment</button>
         <table>
           <tbody>
             {tableRows}
           </tbody>
         </table>
-        <button id={appt_id} className='delete-appt-button' onClick={() => deleteAppt()}>Delete Appointment</button>
+        <button id={id} className='delete-appt-button' onClick={deleteAppt}>Delete Appointment</button>
       </div>
     );
   } 
