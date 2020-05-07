40fc52132a8382dc481cd2c537afa13f6216291b
Shelly <pxshelly@gmail.com>
2020-05-06 22:41:46 -0700

    Protect employee routes

--- client/src/components/employee/Routes.jsx
+++ client/src/components/employee/Routes.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect} from 'react-router-dom';
 import Calendar from './Calendar.jsx';
 import ApptForm  from './ApptForm.jsx';
+import PendingAppts from './PendingAppts.jsx';
+import Login from '../shared/Login.jsx';
+import Logout from '../shared/Logout.jsx';
 
-function Routes() {
+function Routes(props) {
   return (
     <Switch>
-      <Route exact path="/calendar" component={Calendar} />
-      <Route exact path="/create-appointment" component={ApptForm} />
+      <Route exact path="/calendar" render={() => {
+        if (props.loggedIn) {
+          return <Calendar />
+        } else {
+          return <Redirect to='/login'/>
+        }
+      } }/>
+      <Route exact path="/create-appointment" render={() => {
+        if (props.loggedIn) {
+          return <ApptForm />
+        } else {
+          return <Redirect to='/login'/>
+        }
+      } }/>
+      <Route exact path="/pending-appointments" render={() => {
+        if (props.loggedIn) {
+          return <PendingAppts />
+        } else {
+          return <Redirect to='/login'/>
+        }
+      } }/>
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/logged-out" component={Logout} />
     </Switch>
-  )
+  );
 }
 
 export default Routes;
