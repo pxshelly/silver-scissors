56b0d90c63870755d70b54eb01f7193909193b04
Shelly <pxshelly@gmail.com>
2020-06-24 12:34:24 -0700

    Style Logout component

--- client/src/components/shared/Logout.jsx
+++ client/src/components/shared/Logout.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Logout = () => {
   return (
-    <div>Come again soon!</div>
+    <div className='logout'>
+      <div className='logout-text'>Successfully logged out</div>
+      <img className='logout-pic' src='https://silver-scissors.s3-us-west-1.amazonaws.com/IMG_6078.jpg' />
+    </div>
   );
 }
 
