288e2d0096fd4e023d04d07c9ca537881b49ad08
Shelly <pxshelly@gmail.com>
2020-06-24 13:02:37 -0700

    Separate customer Homepage from employees

---
+++ client/src/components/customer/Homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Homepage = () => {
+  return (
+    <div className='opening-content opening-content-customer'>
+      <img src='https://silver-scissors.s3-us-west-1.amazonaws.com/IMG_5991.jpg' />
+      <div className='opening-overlay header-container'>
+        <h1>silver scissors</h1>
+        <h2>hair salon</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Homepage;
