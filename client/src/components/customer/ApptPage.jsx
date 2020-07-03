43b736c75a43af8f6c74558e46926da536324e1f
Shelly <pxshelly@gmail.com>
2020-07-03 10:22:16 -0700

    Turn off use of UpcomingAppts

--- client/src/components/customer/ApptPage.jsx
+++ client/src/components/customer/ApptPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import UpcomingAppts from './UpcomingAppts.jsx';
+// import UpcomingAppts from './UpcomingAppts.jsx';
 import PendingAppts from './PendingAppts.jsx';
 import PastAppts from './PastAppts.jsx';
 
