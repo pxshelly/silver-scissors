94d7703418b1d78114df8acf494f637fabffa1b8
Shelly <shelly@Shellys-MacBook-Pro.local>
2020-11-29 17:47:30 -0800

    Update server url

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,2 +1,2 @@
 const isProduction = true;
-export const SERVER_URL = isProduction ? 'https://silver-scissors-server.herokuapp.com/' : 'localhost:3000/';
+export const SERVER_URL = isProduction ? 'https://silver-scissors.herokuapp.com/' : 'localhost:3000/';
